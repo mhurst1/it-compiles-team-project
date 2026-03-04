e63e3ab6ca5ab6016b74b13f6647f3817d452fbb
eharshyne <eharshyne24@archmereacademy.com>
2026-03-04 10:07:12 -0500

    finished UserList

--- src/main/java/com/interviews/UserList.java
+++ src/main/java/com/interviews/UserList.java
@@ -41,18 +41,19 @@ public class UserList {
     // You Cant Use this. if the feild doesn't exist within the class
     public void editUser(String firstName, String lastName, String username, 
         String password, String email, int graduationYear, String idUSC){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.username = username;
-        this.password = password;
-        this.email = email;
-        this.graduationYear = graduationYear;
-        this.idUSC = idUSC;
+        if(users.contains(user)){
+            user.setFirstName(firstName);
+            user.setLastName(lastName);
+            user.setUsername(username);
+            user.setPassword(password);
+            user.setEmail(email);
+            user.setGraduationYear(graduationYear);
+            user.setIdUSC(idUSC);
+        }
     }
 
     public void save(){
-
-        
+        DataWriter.saveUsers();
     }
 
 
