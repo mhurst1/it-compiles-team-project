d248a2b4a19abd5d4d586fb919d9c87d469375bc
eharshyne <eharshyne24@archmereacademy.com>
2026-03-15 21:32:08 -0400

    java doc Admin.java  and Contributor.java

--- src/main/java/com/interviews/Admin.java
+++ src/main/java/com/interviews/Admin.java
@@ -2,18 +2,30 @@ package com.interviews;
 
 import java.util.UUID;
 
+/**
+ * A class that is just like a User and Contributor, but has more advanced 
+ * administrative features and acessibility 
+ */
 public class Admin extends Contributor {
 
     public Admin(){
         super("Admin", "Admin", "Admin", "Admin", "Admin", 2024, "Admin");
     }
 
+    /**
+     * A method to make a regular user a contributor
+     * @param id the unique UUID of the user
+     */
     public void addContributor(UUID id){
         User user = UserList.getInstance().getUserByID(id);
         if(user!=null)
             user.setStatus(Status.CONTRIBUTOR);
     }
 
+    /**
+     * Method that removes the status of the contributor
+     * @param id the unique UUID of the user
+     */
     public void removeContributor(UUID id){
         User user = UserList.getInstance().getUserByID(id);
         if(user!=null)
--- src/main/java/com/interviews/Contributor.java
+++ src/main/java/com/interviews/Contributor.java
@@ -1,7 +1,11 @@
 package com.interviews;
 
 import java.util.ArrayList;
+import java.util.UUID;
 
+/**
+ * A class that is like the regular User but can add Questions
+ */
 public class Contributor extends User{
 
     private ArrayList<Question> postedQuestions;
@@ -13,15 +17,38 @@ public class Contributor extends User{
         this.postedQuestions = new ArrayList<>(); // Should I be initializing this?? 
     }
 
+    /**
+     * Method that adds a user created question 
+     * @param title the title of the question
+     * @param user the user who created the question
+     * @param description the decription associated with the question
+     * @param questionContent the content in the question
+     * @param hints the hints given to the user 
+     * @param difficulty the difficulty of the question
+     * @param questionLanguage the language the question is written in
+     * @return the newly created question
+     */
     public Question addQuestion(String title, User user, String description,
-                        ArrayList<Section> quesitonContent, ArrayList<String> hints, 
+                        ArrayList<Section> questionContent, ArrayList<String> hints, 
                         Difficulty difficulty, Language questionLanguage){
 
-            Question question = new Question(title, user, description, difficulty, questionLanguage, hints, quesitonContent);
+            Question question = new Question(title, user, description, difficulty, questionLanguage, hints, questionContent);
             postedQuestions.add(question); // Adds question to the posted questions list
             return question;
     }
 
+    /**
+     * A method to update/edit the currently selected question
+     * @param id the questions unique id
+     * @param title the title of the question
+     * @param user the user who created the question
+     * @param description the decription associated with the question
+     * @param questionContent the content in the question
+     * @param hints the hints given to the user 
+     * @param difficulty the difficulty of the question
+     * @param questionLanguage the language the question is written in
+     * @return the newly updated question
+     */
     public Question editQuestion(UUID id, String title, User user, String description, 
         ArrayList<Section> questionContent, ArrayList<String> hints, 
         Difficulty difficulty, Language questionLanguage){
@@ -36,6 +63,10 @@ public class Contributor extends User{
             }
     }
 
+    /**
+     * A method to delete the current question
+     * @param id the unique id of the current question
+     */
     public void removeQuestion(UUID id){
         for(Question question : questions){
             if(question.getId.equals(id)){
