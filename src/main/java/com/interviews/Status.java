fcb7a1fe519eb39a5cec03bd412f18213d281c8c
eharshyne <eharshyne24@archmereacademy.com>
2026-03-17 13:35:48 -0400

    java doc Status.java

--- src/main/java/com/interviews/Status.java
+++ src/main/java/com/interviews/Status.java
@@ -1,5 +1,9 @@
 package com.interviews;
 
+/**
+ * An enumeration to determine which status the user which controls whether 
+ * they can commit questions or not  
+ */
 public enum Status {
     USER,
     CONTRIBUTOR,
