360653204cf020643f6f3e33c28cc26203617610
eharshyne <eharshyne24@archmereacademy.com>
2026-03-17 13:59:45 -0400

    java doc contentType and Difficulty

--- src/main/java/com/interviews/Status.java
+++ src/main/java/com/interviews/Status.java
@@ -1,7 +1,6 @@
 package com.interviews;
 
 /**
- * MH and SE
  * This enum represents the different statuses that a user can have in the application. The possible statuses are USER, CONTRIBUTOR, and ADMIN. Each status may have different permissions and access levels within the application, allowing for role-based functionality and features. This enum can be used to manage user roles and control access to certain features based on the user's status.
  * USER: This status represents a regular user of the application who can view and interact with content but may have limited permissions for creating or modifying content.
  * CONTRIBUTOR: This status represents a user who has the ability to contribute content to the application, such as adding questions or comments. Contributors may have more permissions than regular users but may still have some restrictions compared to admins.
