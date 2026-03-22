1d30f9e6ce308beb833e56c05890eb226f6abc11
eharshyne <eharshyne24@archmereacademy.com>
2026-03-22 11:18:19 -0400

    added jimmy to the json and started the driver

--- src/main/java/com/model/JimmyDriver.java
+++ src/main/java/com/model/JimmyDriver.java
@@ -1,39 +1,44 @@
 package com.model;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import com.interviews.Achievement;
 import com.interviews.Difficulty;
 import com.interviews.Language;
+import com.interviews.Question;
+
+import java.util.ArrayList;
+import java.util.UUID;
 import com.interviews.QuestionApplication;
+import com.interviews.Status;
+import com.interviews.User;
 
 
 public class JimmyDriver {
 
-    private QuestionApplication questionApplication;
+    QuestionApplication app = new QuestionApplication();
 
-    public JimmyDriver(){
-        questionApplication = new QuestionApplication();
+    //cant be tailored to 8 days, it needs to be applicable to other accounts
+    Achievement jimmyAchievement = new Achievement();
+
+    for(int i = 0; i < 8; i++){
+        jimmyAchievement.streakCounter();
     }
 
-    public void run(){
-        scenario();
+    ArrayList<Question> starredQ = new ArrayList<>();
+    ArrayList<Question> answeredQ = new ArrayList<>();
+    ArrayList<Achievement> achievements = new ArrayList<>();
+    achievements.add(JimmyAchievement);
+
+    User jimmy = new User(UUID.randomUUID(), "Jimmy", "Cricket", "jcricket", 
+    "ilovecrickets", "jcricker@email.com", starredQ, answeredQ, achievements, Status.USER, 
+    2026, "X12345678");
+
+    app.createAccount("Jimmy", "Cricket", "jcricket", "ilovecrickets",
+     "jcricker@email.com", 2026, "X12345678");
     }
 
-    public void scenario(){
 
-        questionApplication.login("jimmysmith", "imthegoat5");
 
-        if (questionApplication.getCurrentUser() == null) {
-            System.out.println("Sorry we couldn't login.");
-            return;
-        }
-
-        System.out.println("jimmysmith has been successfully logged in");
-
-        //streak 
-        System.out.println(questionApplication.getCurrentQuestion().getSolutionList());
-
-        //pick a solution
-        //create a user?
-        questionApplication.getCurrentUserSolution().addComment("I'm a little confused how you got this", getCurrentUser(), String comment)
-
-    }
 }
