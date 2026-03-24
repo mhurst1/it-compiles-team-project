9a6971489aab3b2c82abe3b91878e429f1c43e77
Matt Hurst <r.matthewhurst@gmail.com>
2026-03-24 14:09:11 -0400

    Class Testing File

---
+++ src/test/java/com/model/TesterTesting.java
@@ -0,0 +1,11 @@
+package com.model;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class TesterTesting {
+    @Test
+    public void testTesting(){
+        assertTrue(true);
+    }
+}
